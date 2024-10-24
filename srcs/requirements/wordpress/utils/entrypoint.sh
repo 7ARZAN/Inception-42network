ad33ac7d4eea24caa197b4772c049297f5171015
elakhfif <elakhfif@vbox.myguest.virtualbox.org>
2024-10-24 19:12:45 +0100

    im about losin my mind :(

--- srcs/requirements/wordpress/utils/entrypoint.sh
+++ srcs/requirements/wordpress/utils/entrypoint.sh
@@ -6,38 +6,11 @@
 #    By: elakhfif <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/10/13 04:26:29 by elakhfif          #+#    #+#              #
-#    Updated: 2024/10/24 19:02:43 by elakhfif         ###   ########.fr        #
+#    Updated: 2024/10/24 19:10:04 by elakhfif         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
-
-# source /run/secrets/database-credentials
-# source /run/secrets/wordpress-credentials
-#
-# sed -i "s/\$DB_NAME/$DB_NAME/" /www/wp-config.php
-# sed -i "s/\$DB_USER/$DB_USER/" /www/wp-config.php
-# sed -i "s/\$DB_PASSWD/$DB_PASSWD/" /www/wp-config.php
-#
-# sed -i "s/\$AUTH_KEY/$AUTH_KEY/" /www/wp-config.php
-# sed -i "s/\$SECURE_AUTH_KEY/$SECURE_AUTH_KEY/" /www/wp-config.php
-# sed -i "s/\$LOGGED_IN_KEY/$LOGGED_IN_KEY/" /www/wp-config.php
-# sed -i "s/\$NONCE_KEY/$NONCE_KEY/" /www/wp-config.php
-# sed -i "s/\$AUTH_SALT/$AUTH_SALT/" /www/wp-config.php
-# sed -i "s/\$SECURE_AUTH_SALT/$SECURE_AUTH_SALT/" /www/wp-config.php
-# sed -i "s/\$LOGGED_IN_SALT/$LOGGED_IN_SALT/" /www/wp-config.php
-# sed -i "s/\$NONCE_SALT/$NONCE_SALT/" /www/wp-config.php
-# sed -i "s/\$DB_PREFIX/$DB_PREFIX/" /www/wp-config.php
-#
-# wp core install --path="/www" --url="$WEBSITE_HOST" --title="$WEBSITE_TITLE" --admin_user="$WP_MASTER" \
-# --admin_password="$WP_MASTER_PASSWD" --admin_email="$WP_MASTER_EMAIL" --skip-email
-#
-# wp user create $WP_MODERATOR $WP_MODERATOR_EMAIL --role="editor" \
-# --user_pass="$WP_MODERATOR_PASSWD" --path="/www"
-#
-# echo -e "#!/bin/sh\n\nexec php-fpm82 --allow-to-run-as-root -F" > launch.sh
-#
-# exec php-fpm82 --allow-to-run-as-root -F
 
 source /run/secrets/database-credentials
 source /run/secrets/wordpress-credentials
