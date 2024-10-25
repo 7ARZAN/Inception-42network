f1ccfc29de62f368b92946d6b4185814cfa23b2e
elakhfif <elakhfif@vbox.myguest.virtualbox.org>
2024-10-25 20:26:31 +0100

    hopefully

--- srcs/requirements/bonus/vsftpd/utils/entrypoint.sh
+++ srcs/requirements/bonus/vsftpd/utils/entrypoint.sh
@@ -6,18 +6,18 @@
 #    By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/30 17:38:39 by tarzan            #+#    #+#              #
-#    Updated: 2024/10/25 19:09:08 by elakhfif         ###   ########.fr        #
+#    Updated: 2024/10/25 19:48:40 by elakhfif         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-source /run/secrets/ftp_credentials
+source /run/secrets/ftp-credentials
 
 adduser $FTP_USER -Dh /www
 
 echo "$FTP_USER:$FTP_PASSWD" | chpasswd
 
-echo -e "#!/bin/sh\n\nexec vsftpd /vsftpd.conf" > entrypoint.sh
+echo -e "#!/bin/sh\n\nexec vsftpd /vsftpd.conf" > launch.sh
 
 exec vsftpd /vsftpd.conf
