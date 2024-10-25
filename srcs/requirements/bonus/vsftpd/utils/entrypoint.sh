8edac42cc4c1ff0fb71902cc9287673c34a27167
elakhfif <elakhfif@vbox.myguest.virtualbox.org>
2024-10-25 19:11:30 +0100

    i think im losin my mind

--- srcs/requirements/bonus/vsftpd/utils/entrypoint.sh
+++ srcs/requirements/bonus/vsftpd/utils/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: tarzan <elakhfif@student.1337.ma>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/09/30 17:38:39 by tarzan            #+#    #+#              #
-#    Updated: 2024/10/25 03:13:09 by elakhfif         ###   ########.fr        #
+#    Updated: 2024/10/25 19:09:08 by elakhfif         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,6 +18,6 @@ adduser $FTP_USER -Dh /www
 
 echo "$FTP_USER:$FTP_PASSWD" | chpasswd
 
-echo -e "#!/bin/sh\n\nexec vsftpd /vsftpd.conf" > launch.sh
+echo -e "#!/bin/sh\n\nexec vsftpd /vsftpd.conf" > entrypoint.sh
 
 exec vsftpd /vsftpd.conf
